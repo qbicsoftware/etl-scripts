4747205f8576a77a08d54e645fe3c7b097bf376d
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2016-01-18 19:03:40 +0100

    prints for debugging

--- reporting-plugins/update-sample-metadata/script.py
+++ reporting-plugins/update-sample-metadata/script.py
@@ -1,21 +1,15 @@
-import sys
-
-sys.path.append('/abi-projects/QBiC/scripts')
-
 from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchCriteria
-from java.lang import Byte
-from java.io import FileInputStream
-from java.io import File
-import jarray
 
 def process(tr, parameters, tableBuilder):
   ids = parameters.get("identifiers")
   search_service = tr.getSearchService()
   expCodes = []
   if "Experiment" in parameters:
+    print "preparing experiment update"
     for exp in search_service.listExperiments(parameters.get("Project")):
       expCodes.append(exp.getExperimentIdentifier().split("/")[-1])
   for id in ids:
+    print "searching id "+id
     entity = None
     if "Experiment" in parameters and id in expCodes:
       entity = tr.getExperimentForUpdate(parameters.get("Project")+"/"+id)
@@ -23,13 +17,18 @@ def process(tr, parameters, tableBuilder):
       sc = SearchCriteria()
       sc.addMatchClause(SearchCriteria.MatchClause.createAttributeMatch(SearchCriteria.MatchClauseAttribute.CODE, id))
       found = search_service.searchForSamples(sc)
+      print "found: "+str(found)
       if found.size() > 0:
         entity = tr.getSampleForUpdate(id)
     if entity:
       for type in parameters.get("types"):
+        print "handling type "+type
         typeMap = parameters.get(type)
+        print typeMap
         try:
           value = typeMap.get(id)
+          print "value "+value
           entity.setPropertyValue(type,value)
         except:
+          print "exception when trying to set property value!"
           pass
