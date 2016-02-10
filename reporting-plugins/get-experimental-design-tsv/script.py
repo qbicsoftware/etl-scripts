dc6133813187de273fa4092b02c26c56afcb4af8
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2016-02-10 14:55:49 +0100

    remove debug print

--- reporting-plugins/get-experimental-design-tsv/script.py
+++ reporting-plugins/get-experimental-design-tsv/script.py
@@ -77,7 +77,6 @@ def process(tr, parameters, tableBuilder):
 		sample = sampleMap[id]
 		if sample.getSampleType() in types:
 			code = sample.getCode()
-			print "adding sample info for "+code+" to table"
 			row = tableBuilder.addRow()
 			row.setCell(CODE, code)
 			row.setCell(SECONDARY_NAME, sample.getPropertyValue("Q_SECONDARY_NAME"))
