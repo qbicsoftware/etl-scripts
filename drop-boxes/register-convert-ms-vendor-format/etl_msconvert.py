90ec7355a010d12bbc3853e0ba2700375bfd6ba6
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2016-03-07 18:59:13 +0100

    path join

--- drop-boxes/register-convert-ms-vendor-format/etl_msconvert.py
+++ drop-boxes/register-convert-ms-vendor-format/etl_msconvert.py
@@ -451,7 +451,7 @@ def handleImmunoFiles(transaction):
         transaction.moveFile(os.path.join(incomingPath, fileName), newDataSet)
 
         tmpdir = tempfile.mkdtemp(dir=MZML_TMP)
-        raw_path = os.path.join(incomingPath, fileName)
+        raw_path = os.path.join(incomingPath, os.path.join(name, fileName))
         try:
             convert = partial(convert_raw,
                       remote_base=REMOTE_BASE,
