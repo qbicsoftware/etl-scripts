6d328842cc039a0b252dec08148becc19ad57f12
Erhan Kenar <kenar@erhans-mbp.am10.uni-tuebingen.de>
2016-11-21 16:47:07 +0100

    new stuff

--- drop-boxes/register-imaging-dropbox/register-imaging-dropbox.py
+++ drop-boxes/register-imaging-dropbox/register-imaging-dropbox.py
@@ -240,7 +240,7 @@ def process(transaction):
 
     if tissue == 'liver':
         imagingSample.setPropertyValue('Q_IMAGED_TISSUE', 'LIVER')
-    else if tissue = 'tumor':
+    else if tissue == 'tumor':
         imagingSample.setPropertyValue('Q_IMAGED_TISSUE', 'HEPATOCELLULAR_CARCINOMA')
 
     # create new dataset
