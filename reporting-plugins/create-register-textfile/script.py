8858a05bd1941fc2994c30da66c4dcd42d434461
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2017-06-08 18:28:22 +0200

    fix typo

--- reporting-plugins/create-register-textfile/script.py
+++ reporting-plugins/create-register-textfile/script.py
@@ -5,12 +5,12 @@ def process(tr, parameters, tableBuilder):
   file_name = parameters.get("filename")
   lines = parameters.get("content")
   dropbox = "/mnt/DSS1/openbis_dss/"+parameters.get("dropbox")+"/"
-  f_path = os.path.join(dropbox+filename)
+  f_path = os.path.join(dropbox+file_name)
   f = open(f_path, "w")
   for line in lines:
 	f.write(line+"\n")
   f.close()
-  marker_path = os.path.join(dropbox, "MARKER_is_finished_"+filename)
+  marker_path = os.path.join(dropbox, "MARKER_is_finished_"+file_name)
   print f_path
   print marker_path
   #marker = open(marker_path, 'a')
