0d4c8ac8540da5d7b233dbc528fc3e92385c6d17
mohr <mohr@informatik.uni-tuebingen.de>
2017-06-30 13:38:43 +0200

    debug

--- drop-boxes/register-imgag-dropbox/register-imgag.py
+++ drop-boxes/register-imgag-dropbox/register-imgag.py
@@ -181,13 +181,12 @@ def find_and_register_vcf(transaction, jsonContent, varcode):#varcode example: G
                     extID = samp.getPropertyValue("Q_EXTERNALDB_ID")
 
                     if code == 'QMSHS034BK':
-                        print(sampleType)
+                        print(sType)
                         print(curSecName)
-                        print(extDB)
+                        print(extID)
                         print(geneticID)
-                        print(testParentID in s.getParentSampleIdentifiers())
                         print (curSecName != None) and (geneticID in curSecName)
-                        print (extDB != None) and (geneticID in extDB)
+                        print (extID != None) and (geneticID in extID)
 
                     # we are looking for either the test sample with this barcode OR a test sample with parent with this barcode, the right analyte (e.g. DNA) and the short genetics ID in secondary name or external ID
                     if ((barcode == code) and (sType == "Q_TEST_SAMPLE")) or ((qbicBarcodeID in parentIDs) and (analyte == typesDict[expType]) and ((genShortID in curSecName) or (genShortID in extID))):
