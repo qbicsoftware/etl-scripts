be0aafeac0bce5669c7c42a7fa4be42dd983d3ed
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-12-20 13:26:26 +0100

    debugging

--- reporting-plugins/register-sample-batch/script.py
+++ reporting-plugins/register-sample-batch/script.py
@@ -18,7 +18,9 @@ class SampleAlreadyExistsError(Exception):
 def process(tr, params, tableBuilder):
   ignore_existing = "IGNORE EXISTING" in params
   if "user" in params:
-    tr.setUserId(params.get("user"))
+    print "user calling register-sample-batch:"
+    print params.get("user")
+    #tr.setUserId(params.get("user"))
     params.remove("user")
   for sample in params.keySet():
     parameters = params.get(sample)
