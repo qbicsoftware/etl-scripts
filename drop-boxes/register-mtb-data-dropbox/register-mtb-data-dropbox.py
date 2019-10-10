dce307c7c865edf9f8a535f0328d14bd84beb36c
Sven F <sven.fillinger@mailbox.org>
2019-10-10 10:28:31 +0200

    Introduces strategy for different use cases in the mtb context

--- drop-boxes/register-mtb-data-dropbox/register-mtb-data-dropbox.py
+++ drop-boxes/register-mtb-data-dropbox/register-mtb-data-dropbox.py
@@ -178,18 +178,13 @@ def process(transaction):
             proc_mtb(in_file, transaction)
         else:
             unknown_file_types.append(in_file)
-
-    for vcf in vcf_files:
-        register_vcf(vcf, transaction)
-    
-    if fastqs_normal and fastqs_tumor:
-        proc_fastq(fastqs_tumor, transaction)
-        proc_fastq(fastqs_normal, transaction)
-    if len(fastqs_tumor) != 2 or len(fastqs_normal) != 2:
-        raise mtbutils.MTBdropboxerror("Tumor/normal fastq dataset was not complete. Please check.")
-
+            
+    if vfc_files:
+        execute_vcf_registration(vcf_files, transaction)
+    if fastqs_normal or fastqs_tumor:
+        execute_fastq_registration(fastqs_normal, fastqs_turmor, transaction)
     if rna_seq_files:
-        register_rnaseq(rna_seq_files, transaction)
+        execute_rnaseq_registration(rna_seq_files, transaction)
 
     # Check, if there are files of unknown type left
     if unknown_file_types:
@@ -197,9 +192,25 @@ def process(transaction):
             print(mtbutils.log_stardate('Unknown file type: {}'.format(file_name)))
             if file_name.endswith('fastq') or file_name.endswith('fastq.gz'):
                 raise mtbutils.MTBdropboxerror("Unassigned fastq file found: " + file_name)
-                
-
+               
     print(mtbutils.log_stardate('Processing finished.'))
+
+
+def execute_vcf_registration(vcf_files, transaction):
+    for vcf in vcf_files:
+        register_vcf(vcf, transaction)
+
+
+def execute_fastq_registration(fastqs_normal, fastqs_turmor, transaction):
+   if len(fastqs_tumor) != 2 or len(fastqs_normal) != 2:
+        raise mtbutils.MTBdropboxerror("Tumor/normal fastq dataset was not complete. Please check.")
+    else:
+        proc_fastq(fastqs_tumor, transaction)
+        proc_fastq(fastqs_normal, transaction)
+
+
+def execute_rnaseq_registration(rna_seq_files, transaction):
+    register_rnaseq(rna_seq_files, transaction)
 
 
 def get_last_exp_id(experiments):
