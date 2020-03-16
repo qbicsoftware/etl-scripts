e1332883e0b88b37ee4e4f63f473f4c54b5ba218
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-03-16 16:25:47 +0100

    Tracks sample information for fastq

--- drop-boxes/register-fastq-dropbox/register-fastq-dropbox.py
+++ drop-boxes/register-fastq-dropbox/register-fastq-dropbox.py
@@ -19,6 +19,22 @@ from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchSubCriteria
 # *Q[Project Code]^4[Sample No.]^3[Sample Type][Checksum]*.*
 pattern = re.compile('Q\w{4}[0-9]{3}[a-zA-Z]\w')
 
+######## Sample Tracking related import
+from life.qbic.sampletracking import SampleTracker
+from life.qbic.sampletracking import ServiceCredentials
+from java.net import URL
+
+import sample_tracking_helper_qbic as tracking_helper
+#### Setup Sample Tracking service
+SERVICE_CREDENTIALS = ServiceCredentials()
+SERVICE_CREDENTIALS.user = tracking_helper.get_service_user()
+SERVICE_CREDENTIALS.password = tracking_helper.get_service_password()
+SERVICE_REGISTRY_URL = URL(tracking_helper.get_service_reg_url())
+QBIC_LOCATION = tracking_helper.get_qbic_location_json()
+
+### We need this object to update the sample location later
+SAMPLE_TRACKER = SampleTracker.createQBiCSampleTracker(SERVICE_REGISTRY_URL, SERVICE_CREDENTIALS, QBIC_LOCATION)
+
 def isExpected(identifier):
         try:
                 id = identifier[0:9]
@@ -111,3 +127,5 @@ def process(transaction):
                 origName = nameFile.readline().strip()
                 nameFile.close()
         transaction.moveFile(incomingPath, dataSet)
+
+        SAMPLE_TRACKER.updateSampleLocationToCurrentLocation(identifier)
