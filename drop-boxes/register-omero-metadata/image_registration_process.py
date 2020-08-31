10f79e7e5bd8cafde3377b7ea79f382129d8973c
Steffengreiner <Steffen.Greiner@gmx.de>
2020-08-31 13:21:09 +0200

    Remove method implementation since they will be called from backedninterface.py explicitly

--- drop-boxes/register-omero-metadata/image_registration_process.py
+++ drop-boxes/register-omero-metadata/image_registration_process.py
@@ -37,69 +37,10 @@ class ImageRegistrationProcess:
 
     #ToDo If we want to compare on Project Level then should we transfer the project Id and sample code or just iterate over all projects?
     def requestOmeroDatasetId(self, openbis_sample_code):
-
-        omero_dataset_id = -1
-        found_id = False
-
-        for project in self.getObjects("Project"):
-
-            if found_id:
-                break
-
-            #ToDo  Is this coherent between OpenBis and Omero, then we should provide the project ID?
-            if project.getName() == openbis_project_id:
-                for dataset in project.listChildren():
-
-                    if dataset.getName() == openbis_sample_code:
-                        omero_dataset_id = dataset.getId()
-
-                        found_id = True
-                        break
-
-        return omero_dataset_id
+        pass
 
     #ToDo Should the Image be provided via File or as an Array? This Code was originally written for File Input and calls the Omero Import Tool from the commandline
     def registerImageInOmero(self, omero_dataset_id):
-
-        if omero_dataset_id != -1:
-
-
-            #ToDo How should we handle the transaction process here? we need to provide host port user and pwd to connect to Omero. Can we extract that from the current transaction.
-
-            cmd = "omero import -s " + host + " -p " + str(port) + " -u " + usr + " -w " + pwd + " -d " + str(
-                int(omero_dataset_id)) + " " + file_path
-            print("----cmd: " + cmd)
-
-            proc = subprocess.Popen(cmd,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    shell=True,
-                                    universal_newlines=True)
-
-            std_out, std_err = proc.communicate()
-
-            print("std_out: " + std_out)
-            print("std_err: " + std_err)
-            print("return_code: " + str(proc.returncode))
-
-            if int(proc.returncode) == 0:
-
-                print("-->" + std_out)
-
-                fist_line = std_out.splitlines()[0]
-                image_ids = fist_line[6:].split(',')
-
-                print("id list: " + str(image_ids))
-
-            else:
-                print("return code fail")
-
-        else:
-            print("invalid sample_id")
-
-        return image_ids
-
-
         pass
 
     #ToDo Not Implemented Yet since we don't know how the metadata will look like
