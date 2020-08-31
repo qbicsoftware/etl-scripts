fd3cb928dd3f956522167cd6faca37d8c7fb5af2
Steffengreiner <Steffen.Greiner@gmx.de>
2020-08-31 11:36:15 +0200

    Add ToDos and Methods implementation from backendinterface.py

--- drop-boxes/register-omero-metadata/image_registration_process.py
+++ drop-boxes/register-omero-metadata/image_registration_process.py
@@ -6,6 +6,7 @@ import ch.systemsx.cisd.etlserver.registrator.api.v2
 
 from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchCriteria
 from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchSubCriteria
+import subprocess
 
 class ImageRegistrationProcess:
 
@@ -34,18 +35,82 @@ class ImageRegistrationProcess:
         except:
             return False
 
+    #ToDo If we want to compare on Project Level then should we transfer the project Id and sample code or just iterate over all projects?
     def requestOmeroDatasetId(self, openbis_sample_code):
-        pass
 
+        omero_dataset_id = -1
+        found_id = False
+
+        for project in self.getObjects("Project"):
+
+            if found_id:
+                break
+
+            #ToDo  Is this coherent between OpenBis and Omero, then we should provide the project ID?
+            if project.getName() == openbis_project_id:
+                for dataset in project.listChildren():
+
+                    if dataset.getName() == openbis_sample_code:
+                        omero_dataset_id = dataset.getId()
+
+                        found_id = True
+                        break
+
+        return omero_dataset_id
+
+    #ToDo Should the Image be provided via File or as an Array? This Code was originally written for File Input and calls the Omero Import Tool from the commandline
     def registerImageInOmero(self, omero_dataset_id):
+
+        if omero_dataset_id != -1:
+
+
+            #ToDo How should we handle the transaction process here? we need to provide host port user and pwd to connect to Omero. Can we extract that from the current transaction.
+
+            cmd = "omero import -s " + host + " -p " + str(port) + " -u " + usr + " -w " + pwd + " -d " + str(
+                int(omero_dataset_id)) + " " + file_path
+            print("----cmd: " + cmd)
+
+            proc = subprocess.Popen(cmd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    shell=True,
+                                    universal_newlines=True)
+
+            std_out, std_err = proc.communicate()
+
+            print("std_out: " + std_out)
+            print("std_err: " + std_err)
+            print("return_code: " + str(proc.returncode))
+
+            if int(proc.returncode) == 0:
+
+                print("-->" + std_out)
+
+                fist_line = std_out.splitlines()[0]
+                image_ids = fist_line[6:].split(',')
+
+                print("id list: " + str(image_ids))
+
+            else:
+                print("return code fail")
+
+        else:
+            print("invalid sample_id")
+
+        return image_ids
+
+
         pass
 
+    #ToDo Not Implemented Yet since we don't know how the metadata will look like
     def extractMetadataFromTSV(self):
         pass
 
+    #ToDo Not Implemented Yet wait for Andreas?
     def registerExperimentDataInOpenBIS(self, omero_image_ids):
         pass
 
+    #ToDo This can be removed since it's handled in the java Omero-client-lib
     def triggerOMETiffConversion(self):
         pass
 
