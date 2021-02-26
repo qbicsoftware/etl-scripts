f0ba26094f9038e30dbde986a50098d68da4bdf1
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-02-26 15:57:16 +0100

    Documentation/attachments (#65)
    * add readme
    
    * add regex description
    
    * include attachment readme in main readme
    
    Co-authored-by: Sven F <sven.fillinger@qbic.uni-tuebingen.de>

--- drop-boxes/register-attachments-dropbox/register-attachment-dropbox.py
+++ drop-boxes/register-attachments-dropbox/register-attachment-dropbox.py
@@ -92,18 +92,6 @@ def process(transaction):
 		sa.setExperiment(exp)
 	info = None
 
-	#if isProject:
-	#experiments = search_service.listExperiments("/" + space + "/" + project)
-	#for e in experiments:
-	#	if project+"_INFO" in e.getExperimentIdentifier():
-	#		info = e
-	#if not info:
-	#	info = transaction.createNewExperiment('/' + space + '/' + project + '/'+ project+'_INFO', "Q_PROJECT_DETAILS")
-	#else:
-	#	info = transaction.getExperiment('/' + space + '/' + project + '/' + code)
-	# register new experiment and sample
-	#sa.setExperiment(info) 
-	# create new dataset 
 	dataSet = transaction.createNewDataSet("Q_PROJECT_DATA")
 	dataSet.setMeasuredData(False)
 	dataSet.setPropertyValue("Q_SECONDARY_NAME", secname)
