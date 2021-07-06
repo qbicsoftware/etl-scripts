667648773deb18769f67983820c7ea3d8ceda147
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-07-06 10:39:49 +0200

    Dropbox for individual and contextual MTB data

---
+++ drop-boxes/register-mtb-projects-dropbox/mtbutils.py
@@ -0,0 +1,30 @@
+""" A utility class, holding helper functions
+for the main dropbox """
+
+import datetime
+import subprocess as sp
+
+MTB_CONVERTER_PATH = '/home/qeana10/bin/miniconda/bin/mtbconverter'
+
+def mtbconverter(cmds):
+    """Tries to activate a given conda environment"""
+    command = [MTB_CONVERTER_PATH] + cmds
+    ret_code = sp.call(command)
+    return ret_code
+
+def log_stardate(msg):
+    """Prints a message nicely with current stardate"""
+    stardate = datetime.datetime.now()
+    return '{} [{}]: {}'.format(stardate.isoformat(), 'mtbconverter', msg)
+
+class MTBdropboxerror(Exception):
+    """A generic Exception class for this dropbox."""
+
+class Counter():
+
+    def __init__(self):
+        self.counter = 1
+    
+    def newId(self):
+        self.counter += 1
+        return self.counter - 1
